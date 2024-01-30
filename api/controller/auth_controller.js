2afd6524a102856957282d282ee0c6097c7bfd16
abdul17097 <abdul17097@gmail.com>
2024-01-30 10:32:34 -0800

    complete signin page functionality

--- api/controller/auth_controller.js
+++ api/controller/auth_controller.js
@@ -16,21 +16,25 @@ const signup = async (req, res, next) => {
     
 }
 
-const signin = async (req, res, next) =>{
-    const {email, password} = req.body;
+const signin = async (req, res, next) => {
+    const { email, password } = req.body;
     try {
-        const validUser = await User.findOne({email});
-        if (!validUser) return next(errorHandler(404, "User not found!"))
-        const isPasswordValid = bcryptjs.compareSync(password, validUser.password); 
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, "User not found!"));
+
+        const isPasswordValid = bcryptjs.compareSync(password, validUser.password);
         if (!isPasswordValid) return next(errorHandler(401, "Wrong Credentials!"));
-        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRETE_KEY);
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRETE_KEY);
+
         res
-        // .cokkie('access_token', token, {httpOnly: true})
-        .status(200)
-        .json(validUser);
+            .cookie('access_token', token, { httpOnly: true })
+            .status(200)
+            .json(validUser);
     } catch (error) {
         next(error);
     }
-}
+};
+
 
 module.exports = {signup, signin};
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
-
-const SignIn = () => {
+import axios from 'axios'
+import React, { useState } from 'react'
+import {Link, useNavigate} from 'react-router-dom'
+const SignIN = () => { 
+    const [formData, setFormData] = useState()
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate()
+    const handleChange = (event) => {
+        setFormData({...formData, [event.target.name]: event.target.value})
+    }
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+        try {
+            setLoading(true);
+            const res = await fetch(`/api/auth/signin`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            }            
+            );
+            const data = await res.json();
+            if(data.success === false) {
+                setError(data.message);
+                setLoading(false);
+                return;
+            }
+            setLoading(false);
+            setError(null);
+            navigate('/')
+        } catch (error) {
+            console.error('Error during signup:', error);
+            setLoading(false);
+            setError(error.message);
+        }
+    }
   return (
-    <div>SignIn</div>
+    <div className='p-3 max-w-lg mx-auto '>
+        <h1 className="text-3xl text-center font-bold my-7">Sign In</h1>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+            <input 
+            type="email"
+            placeholder='email' 
+            className="border p-3 rounded-lg"
+            onChange={handleChange}
+            id='email'  
+            name='email'
+            />
+            <input 
+            type="password"
+            placeholder='password' 
+            className="border p-3 rounded-lg"
+            onChange={handleChange}
+            id='password' 
+            name='password' 
+            />
+            <button disabled={loading} className="bg-slate-700 p-3 rounded-lg text-white uppercase hover:opacity-65 disabl ed:opacity-80 ">{loading? "LOADING...": "Sign IN"}</button>
+        </form>
+        <div className="flex gap-2 mt-5">
+            <p className="">Create an account?</p>
+            <Link to='/signup'>
+                <span className="text-blue-700">Sign UP</span>
+            </Link>
+        </div>
+        {error && <p className="text-red-500">{error}</p>}
+    </div>
   )
 }
 
-export default SignIn
+export default SignIN
