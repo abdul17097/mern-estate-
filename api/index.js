a745bec9349c47cd1c65df9c4b7be16a12c2ff6e
abdul17097 <abdul17097@gmail.com>
2024-01-29 23:31:07 -0800

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
 
+mongoose
+    .connect(process.env.MONGO)
+    .then(()=>{
+        console.log("Connected to Mongo");
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000');
 })
